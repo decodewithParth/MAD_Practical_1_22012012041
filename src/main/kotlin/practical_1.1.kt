4b68138ba97236032ff873ee15fecb93e638ad76
student <student@gnu.ac.in>
2023-07-17 10:26:25 +0530

    doing practical 1

---
+++ src/main/kotlin/practical_1.1.kt
@@ -0,0 +1,42 @@
+fun main(){
+    //println("Hello World!!!")
+    println("1.2")
+    val a:Int=1
+    val b:Char='A'
+    val c:Float=2.0f
+    val d:String="Parth Chauhan"
+    val e:Boolean=true
+    val f:Double=4.2
+    val g:Long=24423
+    val h:Short=12
+    val i:Byte=-10
+    println("Integer value is '$a'")
+    println("Float value is '$c'")
+    println("Character value is '$b'")
+    println("String value is '$d'")
+    println("Boolean value is '$e'")
+    println("Double value is '$f'")
+    println("Long value is '$g'")
+    println("Short value is '$h'")
+    println("Byte value is '$i'")
+
+
+
+    println("1.2")
+    val num:Int=10
+    val num1:Double=num.toDouble()
+    val num2:String="10"
+    val num3:Int=num2.toInt()
+    val num4:Int=num2.toInt()
+    val num5:Double=num2.toDouble()
+    println("Integer value $num")
+    println("Double value (From Integer) $num1")
+    println("String value $num2")
+    println("Integer value1 (From String) $num3")
+    println("Integer value2 (From String) $num4")
+    println("Double value (From String) $num5")
+
+    println("1.3")
+
+
+}
---
+++ src/main/kotlin/practical_1.2.kt
@@ -0,0 +1,14 @@
+fun main(){
+    val num:Int=10
+    val num1:Double=num.toDouble()
+    val num2:String="10"
+    val num3:Int=num2.toInt()
+    val num4:Int=num2.toInt()
+    val num5:Double=num2.toDouble()
+    println("Integer value $num")
+    println("Double value (From Integer) $num1")
+    println("String value $num2")
+    println("Integer value1 (From String) $num3")
+    println("Integer value2 (From String) $num4")
+    println("Double value (From String) $num5")
+}
