594a9aac690f0a3135a5dedaa8722d0d278ce14a
student <student@gnu.ac.in>
2023-07-18 10:28:58 +0530

    18-7-2022 lab

---
+++ src/main/kotlin/Practical_1.8.kt
@@ -0,0 +1,12 @@
+fun main() {
+    var a1 = arrayOf(10, 90, 60, 80, 100);
+    println("Created array-1 using arrayOf method: ")
+    for (item in a1) {
+        println(item)
+    }
+
+    var a4=IntArray(5);
+
+}
+
+
--- src/main/kotlin/practical_1.7.kt
+++ src/main/kotlin/practical_1.7.kt
@@ -5,9 +5,19 @@ fun fact(num:Int):Int{
     else return num*fact(num-1);
 }
 
+tailrec fun factorial(n: Int, run: Int = 1): Long {
+    return if (n == 1){
+        run.toLong()
+    } else {
+        factorial(n-1, run*n)
+    }
+}
 fun main(){
     print("Enter number : ")
     var num= readln()!!.toInt();
     var ans=fact(num);
+    var tail=factorial(5);
     println("Factorial of ${num} = ${ans}")
+    println("By tailrec keyword ,Factorial of ${num} = ${ans}")
+
 }
