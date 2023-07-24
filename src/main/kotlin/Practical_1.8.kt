a956f426f326d97a6a93408f8dff972ad1b99373
student <student@gnu.ac.in>
2023-07-24 10:32:25 +0530

    vcg

--- src/main/kotlin/Practical_1.8.kt
+++ src/main/kotlin/Practical_1.8.kt
@@ -1,11 +1,22 @@
+fun printArray(arr: Array<Int>){
+    for (item in arr) {
+        println(item)
+    }
+}
+
 fun main() {
     var a1 = arrayOf(10, 90, 60, 80, 100);
     println("Created array-1 using arrayOf method: ")
-    for (item in a1) {
-        println(item)
-    }
+    printArray(a1);
 
-    var a4=IntArray(5);
+    var a2=Array<Int>(5){0};
+    println("Created array-2 using Array<>() method: ")
+    printArray(a2);
+
+    var a3=Array<Int>(10){i:Int->i+1};
+    println("Created array-3 using Array<>() and lambda function: ");
+    printArray(a3);
+
 
 }
 
